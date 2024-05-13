010b4f3287af112f73a37f970c00bd4901323731
Maggie Chao <75676764+mfchao@users.noreply.github.com>
2024-05-13 00:18:49 -0400

    scroll indicators

--- src/components/HtmlWrapper.jsx
+++ src/components/HtmlWrapper.jsx
@@ -40,7 +40,7 @@ export const HtmlWrapper = (props) => {
         if (scrollData.scroll.current > 0.51 && scrollData.scroll.current < 0.55) {
             setMapOpened(true);
             setOpenMap('holc');
-        } else if (scrollData.scroll.current > 0.785 && scrollData.scroll.current < 0.81) {
+        } else if (scrollData.scroll.current > 0.785 && scrollData.scroll.current < 0.825) {
             setMapOpened(true);
             setOpenMap('race');
         } else if (scrollData.scroll.current > 0.87 && scrollData.scroll.current < 0.92) {
